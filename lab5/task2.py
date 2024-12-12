6a7b971ea140249dc8d223d6634c91955c15a11c
leonardo_Wer <name89213126414@gmail.com>
2024-12-12 18:16:38 +0300

    Исправлена критическая ошибка

--- lab5/task2.py
+++ lab5/task2.py
@@ -2,3 +2,4 @@ def create_task(title:str, description:str):
     # Логика создания задачи
     print(f"Создана новая задача: {title}")
     print(f"Описание задачи:{description}")
+    print("Исправлена критическая ошибка: сделана лабораторная 5")
